d0ee7d2ba77d029df11f9a9fe7d769c0fa9dd0f8
panterra-000 <panterra-000@mail.ru>
2022-03-20 16:33:13 +0500

    actor section ...

--- presentation/src/main/kotlin/uz/rdo/presentation/screens/ActorDetailScreen.kt
+++ presentation/src/main/kotlin/uz/rdo/presentation/screens/ActorDetailScreen.kt
@@ -4,9 +4,17 @@ import android.widget.Toast
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import uz.rdo.coreui.R
+import uz.rdo.coreui.composable.base.columns.ColumnScrollableFillMaxSize
+import uz.rdo.coreui.composable.views.AppBarViewWithIcons
 import uz.rdo.presentation.viewmodels.ActorDetailViewModel
+import uz.rdo.presentation.viewmodels.MovieDetailViewModel
+import uz.rdo.remote.data.response.actor.ActorDetailResponse
+import uz.rdo.remote.data.response.actor.MovieCastItem
+import uz.rdo.remote.data.response.movie.ActorCastItem
 
 @Composable
 fun ActorDetailScreen(
@@ -31,6 +39,26 @@ fun ActorDetailScreen(
         }
     })
 
+    ActorDetailScreenView(viewModel = viewModel, backClick = {
+        navController.navigateUp()
+    }, castClick = {
+
+    })
+
+}
+
+@Composable
+fun ActorDetailScreenView(
+    viewModel: ActorDetailViewModel,
+    backClick: () -> Unit,
+    castClick: (MovieCastItem) -> Unit
+) {
+    ColumnScrollableFillMaxSize {
+        AppBarViewWithIcons(title = stringResource(R.string._title_detail),
+            startIconClick = {
+                backClick()
+            })
 
 
+    }
 }
